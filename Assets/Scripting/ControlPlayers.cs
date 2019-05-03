0a39560254c3e4f1e35b151a60551d958b494524
Usaka Rokujou <usaka_rokujou@outlook.com>
2019-05-03 17:18:59 -0500

    Add class to controller the input

---
+++ Assets/Scripting/ControlPlayers.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlayers : MonoBehaviour {
+
+	/*
+		The players need control the token using their hands, however, we would use a keyinput to manager
+		 game, after, the game input will be swap Leap Motion
+	 */
+
+	/*
+		The user have 7 controlles:
+		1 Change token onward
+		2 Change token backward
+		3 Rotate the camera clockwise
+		4 Rotate the camera counterclockwise
+		5 Select 
+		6 Roll the dice
+		7 "Soplar" the player after moved its tokens
+
+		Any player have its 7 keys:
+		Player 1
+		1 d
+		2 a
+		3 e
+		4 q
+		5 w
+		6 s
+		7 z
+
+		Player 2
+		1 l
+		2 j
+		3 o
+		4 u
+		5 i
+		6 k
+		7 m
+	*/
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
