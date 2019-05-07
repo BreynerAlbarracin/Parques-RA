d81456218d6a2da435f5b4bb14db51ed05cdf4fa
Usaka Rokujou <usaka_rokujou@outlook.com>
2019-05-06 23:45:01 -0500

    Add value in Dice class

--- Assets/Scripting/Dice.cs
+++ Assets/Scripting/Dice.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Dice : MonoBehaviour {
 
+    int value;
+
     public void throwDice() {
+        this.value = 0;
+        this.transform.SetParent(GameObject.Find("Dices").transform);
         this.transform.localPosition = new Vector3(this.transform.localPosition.x, 15, this.transform.localPosition.z);
         this.transform.localRotation = new Quaternion(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360), 1);
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
     }
 }
