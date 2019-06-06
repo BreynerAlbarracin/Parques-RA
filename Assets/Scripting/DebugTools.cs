ad4e5f02cec0503256c5059bc66bb484eb57c6c2
Usaka Rokujou <usaka_rokujou@outlook.com>
2019-06-06 16:00:16 -0500

    ajustado el debug

--- Assets/Scripting/DebugTools.cs
+++ Assets/Scripting/DebugTools.cs
@@ -14,7 +14,7 @@ public class DebugTools : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (!debugged) {
+        if (debugged) {
             createDebugView();
         }
     }
