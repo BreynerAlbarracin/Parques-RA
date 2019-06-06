25991610cc2eb3d3c0a931c6b609832c9b310ad9
Usaka Rokujou <usaka_rokujou@outlook.com>
2019-06-06 15:59:52 -0500

    Juego para dos personas

--- Assets/Scripting/PlayerManager.cs
+++ Assets/Scripting/PlayerManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
 
-	GameObject[] players;
+    GameObject[] players;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
+        players = new GameObject[2];
+    }
+
+    // Update is called once per frame
+    void Update() {
+        players[0] = GameObject.Find("HandsRed");
+        players[1] = GameObject.Find("HandsYellow");
+        
+        players[0].GetComponent<ActionPlayer>().startTurn();
+
+        if (!players[0].GetComponent<ActionPlayer>().isTurn()) {
+            players[1].GetComponent<ActionPlayer>().startTurn();
+        } else {
+            Debug.Log("Turno player 1");
+        }
+
+        if (!players[1].GetComponent<ActionPlayer>().isTurn()) {
+            players[0].GetComponent<ActionPlayer>().startTurn();
+        }else{
+            Debug.Log("Turno player 2");
+        }
+    }
 }
